ccb886843dbc3f791f279e077bfe43a7132db8e8
eduardo <eduardokegler1@gmail.com>
2019-05-10 01:35:54 -0300

    Setting typescript and apollo workspace

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GITHUB_BASE_URL: string = 'https://api.github.com/graphql';
+const cache: InMemoryCache = new InMemoryCache();
+const httpLink: HttpLink = new HttpLink({
+    uri: GITHUB_BASE_URL,
+    headers: {
+        authorization: `Bearer ${
+            process.env.REACT_APP_GITHUB_PERSONAL_ACCESS_TOKEN
+            }`,
+    },
+});
+
+const client: ApolloClient<any> = new ApolloClient({
+    link: httpLink, cache
+})
+
+
+ReactDOM.render(
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
